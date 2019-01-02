17b8bfd65d04a3a07bdb412ed40a67eff9eba29e
Amir Barak <amirbarak91@gmail.com>
2019-01-02 23:33:54 +0200

    yet another try.

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/material-dashboard-angular'));
+app.use(express.static(__dirname + '/dist/tools-dashboard'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/material-dashboard-angular/index.html'));
+res.sendFile(path.join(__dirname+'/dist/tools-dashboard/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
