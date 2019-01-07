52ffeda909fe69b722d5031daea885dea0e10564
Amir Barak <amirbarak91@gmail.com>
2019-01-07 20:50:52 +0200

    more changes.

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const app_name = 'material-dashboard-angular';
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(`./dist/${app_name}`));
+//app.use(express.static(`./dist/${app_name}`));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname,`/dist/${app_name}/index.html`));
+res.sendFile(path.join(__dirname,`/dist/index.html`));
 });
 
 // Start the app by listening on the default Heroku port
